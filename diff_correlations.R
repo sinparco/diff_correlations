f0f29eeea4eaa4713592d2edcbcd82c092383fb1
Parco Sin <psin@uoguelph.ca>
2016-10-27 21:27:43 -0400

    commit

--- diff_correlations.R
+++ diff_correlations.R
@@ -1,37 +1,39 @@
-library(tidyverse)
+library (tidyverse)
 library(apaTables)
 library(cocor)
 library(predictionInterval)
 
-#Question 1
+## determine correlation between rating-complaint and rating-critical correlation
 r.jk <- .83
 r.jh <- .16
 r.kh <- .19
 n <- 30
 
 cocor.dep.groups.overlap(r.jk, r.jh, r.kh, n)
-#Change in r = .67, 95% CI [.32, 1.04]
 
-#null hypothesis rejected
-
-#QUestion 2
-
-# j = Rating
-# k = Complaint
-# h = Raises
-# m = Critical
-
+## determine correlation between rating-complaint and raises-critical correlation
 r.jk <- .83
+r.hm <- .38
 r.jh <- .59
 r.jm <- .16
-r.hm <- .38
-r.hk <- .67
+r.kh <- .67
 r.km <- .19
 n <- 30
 
-cocor.dep.groups.nonoverlap(r.jk, r.jh, r.kh, n, var.labels=c("rating", "complaint", "critical"))
+cocor.dep.groups.nonoverlap(r.jk, r.hm, r.jh, r.jm, r.kh, r.km, n)
 
-#Question 3
-pi.r(r=.43, n=30, rep.n=100)
+## run a replication of the study
+pi.r(r=.43,n=30,rep.n=100)
 
-#PI = r=.43, 95% PI [.05, .74]
+## figure out replication sample size with PI less than 0.5 difference
+pi.r(r=.43,n=30,rep.n=100)
+pi.r(r=.43,n=30,rep.n=1000)
+pi.r(r=.43,n=30,rep.n=10000)
+
+## Comparing correlations from 2 papers
+r1.jk <- .43
+r2.hm <- .10
+n1=30
+n2=1000
+
+cocor.indep.groups(r1.jk, r2.hm, n1, n2)
